92ff9a8f77626a175e31d9a19fa497babb7e23d8
Tanmay Khandelwal <58903502+Tanmay-901@users.noreply.github.com>
2021-02-11 22:06:57 +0530

    Update device.ino

--- device.ino
+++ device.ino
@@ -5,6 +5,7 @@
 #define RST_PIN         9           // Configurable, see typical pin layout above
 #define SS_PIN          10          // Configurable, see typical pin layout above
 
+
 const byte ROWS = 4; //four rows
 const byte COLS = 4; //three columns
 char keys[ROWS][COLS] = {
@@ -13,58 +14,55 @@ char keys[ROWS][COLS] = {
   {'7','8','9','C'},
   {'*','0','#','D'}
 };
-byte rowPins[ROWS] = {0,2,3,4}; //connect to the row pinouts of the keypad
+byte rowPins[ROWS] = {A0,2,3,4}; //connect to the row pinouts of the keypad
 byte colPins[COLS] = {5,6,7,8}; //connect to the column pinouts of the keypad
 
 Keypad keypad = Keypad( makeKeymap(keys), rowPins, colPins, ROWS, COLS );
 
 MFRC522 mfrc522(SS_PIN, RST_PIN);   // Create MFRC522 instance
 int a=4500, b=5500, c=6500;
-int readsuccess, penalty;
+int readsuccess;
 byte readcard[4];
-char str[32] = "";
-String StrUID, charge;
+char str[32] = "", key;
+String StrUID, charge, penalty;
 
 void setup() {
   Serial.begin(9600);                                           // Initialize serial communications with the PC
   SPI.begin();                                                  // Init SPI bus
   mfrc522.PCD_Init();                                              // Init MFRC522 card
-//  Serial.println(F("Read personal data on a MIFARE PICC:"));    //shows in serial that it is ready to read
+//  Serial.println(F("Read personal data on a MIFARE PICC:"));    //shows in serial that it is ready to read 
 }
 
 void loop() {
 
    readsuccess = getid();
   if(readsuccess) {
-    //Serial.println(StrUID);
    Serial.println("");
-      if(StrUID=="DB692E0B"){
-    Serial.println("Vehicle no: RJ14-LC-1996");
-    Serial.println("Select Charge: ");
-    while(penalty=="" or charge==""){
-      penalty, charge = fine();
-    }
-    a=a-int(penalty);
-    Serial.println("Mr. ADITYA is fined Rs. ");
-    Serial.print(int(penalty));
-    Serial.print(" for lack of ");
-    Serial.print(charge);
-    Serial.print(" !!!");
-    Serial.println("");
-    Serial.println("");
-    penalty = "";
-    charge="";
+    if(StrUID=="DB692E0B"){
+      Serial.println("Vehicle no: RJ14-LC-1996");
+      Serial.print("Select Charge: ");
+      while(penalty=="" or charge==""){
+        penalty, charge = fine();
+      }
+      Serial.println("Mr. ADITYA is fined Rs. ");
+      Serial.print(penalty);
+      Serial.print(" for ");
+      Serial.print(charge);
+      Serial.print(" !!!");
+      Serial.println("");
+      Serial.println("");
+      penalty = "";
+      charge="";
       }
     if(StrUID=="E63C27F8"){
-    b=b-1000;
     Serial.println("Vehicle no: RJ14-CG-3542");
     Serial.println("Select Charge: ");
     while(penalty=="" or charge==""){
       penalty, charge = fine();
     }
-    Serial.println("Mr. Tanmay is fined Rs. ");
-    Serial.print(int(penalty));
-    Serial.print(" for lack of ");
+    Serial.print("Mr. Tanmay is fined Rs. ");
+    Serial.print(penalty);
+    Serial.print(" for ");
     Serial.print(charge);
     Serial.print(" !!!");
     Serial.println("");
@@ -78,12 +76,11 @@ void loop() {
     while(penalty=="" or charge==""){
       penalty, charge = fine();
     }
-    Serial.println("Miss. Shivani is fined Rs. ");
-    Serial.print(int(penalty));
-    Serial.print(" for lack of ");
+    Serial.print("Miss. Shivani is fined Rs. ");
+    Serial.print(penalty);
+    Serial.print(" for ");
     Serial.print(charge);
     Serial.print(" !!!");
-    c=c-int(penalty);
     Serial.println("");
     Serial.println("");
     penalty="";
@@ -100,7 +97,6 @@ int getid() {
   if(!mfrc522.PICC_ReadCardSerial()) {
     return 0;
   }
-  //Serial.println("THE UID OF THE SCANNED CARD IS:");
  
   for(int i=0;i<4;i++){
     readcard[i]=mfrc522.uid.uidByte[i]; //storing the UID of the tag in readcard
@@ -116,11 +112,19 @@ String fine(){
   if (key){
   if (key=='1'){
     penalty = "200";
-    charge = "Helmet" ;
+    charge = "Not wearing Helmet" ;
   }
   if (key=='2'){
-    penalty = "400";
-    charge = "Documents" ;
+    penalty = "1000";
+    charge = "lack of Documents" ;
+  }
+  if (key=='3'){
+    penalty = "1500";
+    charge = "Rash Driving" ;
+  }
+  if (key=='4'){
+    penalty = "500";
+    charge = "Not having Insurance" ;
   }
   }
   else{
